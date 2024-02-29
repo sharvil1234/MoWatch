d44b1aa97c4b4362d12056f9d19c4a38405f2aa5
sharvil1234 <71960080+sharvil1234@users.noreply.github.com>
2024-02-29 15:54:59 +0530

    Add files via upload

--- watch.js
+++ watch.js
@@ -1,3 +1,5 @@
 srcId = localStorage.getItem("srcId");
 console.log(srcId);
+alert("SHARVIL SAYS : CANCEL OUT ANY ADS THAT MIGHT POP IN THE PLAYER UP BY CLICKING ON THE X IN THE CORNER OF THE PLAYER...");
+alert("CLOSE ANY TABS THAT MIGHT OPEN AFTER CLICKING ON THE VIDEO PLAYER ELEMENTS...");
 document.getElementById("watchframe").src = "https://vidsrc.xyz/embed/movie/" + srcId;
