807d6621cf427ffa134c8a7ed6b1eb7568be8490
sharvil1234 <71960080+sharvil1234@users.noreply.github.com>
2024-02-29 15:45:58 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 function navigate() {
     query = document.getElementById("inpId").value;
     localStorage.setItem("query", query);
-    window.location.replace = "https://sharvil1234.github.io/MoWatch/buffer.html";
+    window.location.replace("https://sharvil1234.github.io/MoWatch/buffer.html");
 }
